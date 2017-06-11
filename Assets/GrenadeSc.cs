ef8da2e74a5f0e93181a2f7f609d495a61702dfa
unknown <hakuaki0894@icloud.com>
2017-06-11 15:20:56 +0900

    グレネード

---
+++ Assets/GrenadeSc.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrenadeSc : MonoBehaviour {
+	public GameObject prefab_bom;
+	
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("bom");
+	
+	}
+	IEnumerator bom(){
+		yield return new WaitForSeconds (2.5f);
+		Destroy (gameObject);
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Playersc.cs
+++ Assets/Playersc.cs
@@ -5,6 +5,8 @@ public class Playersc : MonoBehaviour {
 
 	public Camera camera;
 	public AudioSource Gunsound;
+	public GameObject prefab_bom;
+
 
 	int playerHP = 10;
 
@@ -20,13 +22,31 @@ public class Playersc : MonoBehaviour {
 		if (Input.GetMouseButtonDown (0)) {
 			Shot ();
 			Gunsound.Play ();
+		
 		}
+		if(Input.GetMouseButtonDown(1))
+			{
+				BomDamage();
+			}
+
 	}
 	void PlayerDamage(){
 		playerHP -= 1;
 		Debug.Log (playerHP);
 	}
-	
+
+
+	void BomDamage(){
+		Vector3 pos =transform.position + transform.TransformDirection (Vector3.forward);
+		GameObject bom = Instantiate (prefab_bom, pos, Quaternion.identity)as GameObject;
+
+		Vector3 bom_speed = transform.TransformDirection (Vector3.forward) * 5;
+		bom_speed += Vector3.up * 5;
+		bom.GetComponent<Rigidbody> ().velocity = bom_speed;
+
+		bom.GetComponent<Rigidbody> ().angularVelocity = Vector3.forward * 7;
+	}
+
 	void Shot ()
 	{
 		int distance = 50;
